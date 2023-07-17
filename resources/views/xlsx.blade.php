07a94dcac9f40163f56bcb23bfd992b29a1db507
Lordxer0 <megaxero00@gmail.com>
2023-07-17 11:22:31 -0500

    ya funciona y se ve bonito

--- resources/views/xlsx.blade.php
+++ resources/views/xlsx.blade.php
@@ -35,20 +35,17 @@
                                         </svg>
                                     </div>
     
-                                    <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Archivo origen</h2>
+                                    <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white ">Archivo origen</h2>
+                                    <br>
                                     <div>
                                         <input class="block w-full text-sm text-slate-500
                                         file:mr-4 file:py-2 file:px-4
                                         file:rounded-full file:border-0
                                         file:text-sm file:font-semibold
-                                        file:bg-violet-50 file:text-violet-700
-                                        hover:file:bg-violet-100" type="file" name="archivo_origen" id="archivo_origen">
+                                        file:bg-violet-50 hover:file:bg-violet-100" type="file" name="archivo_origen" id="archivo_origen">
                                     </div>
                                 </div>
     
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
-                                </svg>
                             </div>
     
                             <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
@@ -59,26 +56,24 @@
                                         </svg>
                                     </div>
     
-                                    <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Archivo Destino</h2>
-                                    
+                                    <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white ">Archivo Destino</h2>
+                                    <br>
                                     <div>
                                         <input class="block w-full text-sm text-slate-500
                                         file:mr-4 file:py-2 file:px-4
                                         file:rounded-full file:border-0
                                         file:text-sm file:font-semibold
-                                        file:bg-violet-50 file:text-violet-700
-                                        hover:file:bg-violet-100" type="file" name="archivo_destino" id="archivo_destino">
+                                        file:bg-violet-50 hover:file:bg-violet-100" type="file" name="archivo_destino" id="archivo_destino">
                                     </div>
     
                                 </div>
     
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
-                                </svg>
                             </div>
-    
                         </div>   
-                        <button  class="bg-indigo-500" type="submit" >Enviar</button>                     
+                        <br>
+                        <div class="grid">
+                            <button  class="px-4 py-1 text-sm bg-sky-400 font-semibold rounded-full self-center" type="submit" >Enviar</button>                     
+                        </div>
                     </form>
                 </div>
 
